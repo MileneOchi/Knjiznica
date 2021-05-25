9bc2f4062936ba70e96acc34995141ab2e67482b
MileneOchi <lukamiskulin1@gmail.com>
2021-05-25 13:49:21 +0200

    Ostali fileovi

---
+++ Knjiznica/Header.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef struct Book {
+	int id;
+	char ime[30];
+	char autor_ime[20];
+	char autor_prezime[30];
+	char zanr[20];
+	int state;
+	int date[10];
+	struct Book* next;
+	struct Book* prev;
+} BOOK;
+
+typedef struct Member {
+	int id;
+	int book;
+	int books[10];
+	char name[20];
+	char lastName[30];
+	struct Member* next;
+	struct Member* prev;
+} MEMBER;
+
+
+#endif
